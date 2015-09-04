f6c98d0b180a31c882b31775d4cbebfdcdb9c016
tavisca-arsalan <arsalang@tavisca.com>
2015-09-04 13:44:47 +0530

    Seond addition

--- AssignmentB.Tests/UnitTest1.cs
+++ AssignmentB.Tests/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections;
 namespace AssignmentB.Tests
 {
     [TestClass]
@@ -9,6 +9,86 @@ namespace AssignmentB.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            System.Collections.Generic.List<Itinerary> disountList = new System.Collections.Generic.List<Itinerary>();
+
+            Itinerary published=new Itinerary()
+            {
+              OriginAirportCode="001",
+
+              DestinationAirportCode="005",
+
+              FlightTime = new TimeSpan(4,0,0),
+
+              NumberOfStops =1,
+
+              TotalLayoverTime =new TimeSpan(1,0,0),
+
+              Airline ="Indigo",
+
+              UtcDepartureTime = DateTime.UtcNow,
+
+              UtcArrivalTime= DateTime.UtcNow,
+
+              BaseFareInUSD = 100,
+
+              MarkupInUSD =0 
+
+            };
+
+            Itinerary discounted1 = new Itinerary()
+            {
+                OriginAirportCode = "001",
+
+                DestinationAirportCode = "005",
+
+                FlightTime = new TimeSpan(4, 0, 0),
+
+                NumberOfStops = 1,
+
+                TotalLayoverTime = new TimeSpan(1, 0, 0),
+
+                Airline = "Indigo",
+
+                UtcDepartureTime = DateTime.UtcNow,
+
+                UtcArrivalTime = DateTime.UtcNow,
+
+                BaseFareInUSD = 80,
+
+                MarkupInUSD = 0
+
+            };
+
+            disountList.Add(discounted1);
+            Itinerary discounted2 = new Itinerary()
+            {
+                OriginAirportCode = "001",
+
+                DestinationAirportCode = "005",
+
+                FlightTime = new TimeSpan(4, 0, 0),
+
+                NumberOfStops = 2,
+
+                TotalLayoverTime = new TimeSpan(1, 0, 0),
+
+                Airline = "Indigo",
+
+                UtcDepartureTime = DateTime.UtcNow,
+
+                UtcArrivalTime = DateTime.UtcNow,
+
+                BaseFareInUSD = 110,
+
+                MarkupInUSD = 0
+
+            };
+
+            disountList.Add(discounted2);
+
+            disountList = new MarkupManager().CalculateMarkup(published, disountList);
+
+            Assert.AreEqual(1,disountList.Count);
         }
     }
 }
